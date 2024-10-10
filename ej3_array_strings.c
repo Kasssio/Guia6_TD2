00d9fb93eeb0730c2b513162f0a74c9a4bb7f456
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2024-10-10 08:46:29 -0300

    Ejercicio 3 hecho

--- ej3_array_strings.c
+++ ej3_array_strings.c
@@ -1,33 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int len(char* s) {
+    int length = 0;
+    while (s[length] != '\0'){
+        length++;
+    }
+    return length;
+}
 char* longest(char* v[], int size) {
-
-    // COMPLETAR
-
-    return 0;
+    char* longestChar = v[0];
+    for(int i = 0;i < size; i++){
+        for (int j=0;j< size - i;j++){
+            if(len(v[j]) > len(longestChar)){
+                longestChar = v[j];
+            }
+        }
+    }
+    return longestChar;
 }
 
 char* superConcatenate(char* v[], int size) {
+    int totalSize = 0;
+    int resIndex = 0;
+    for (int i=0;i<size;i++){
+        totalSize += len(v[i]); // Saco la longitud total necesaria
+    }
+    char* res = (char*) malloc(sizeof(char) * totalSize);
+    for (int i=0;i<size;i++){
+        char* strActual = v[i];
+        for(int j = 0; strActual[j] != '\0'; j++, resIndex++){
+            res[resIndex] = strActual[j];
+        }
+    }
 
-    // COMPLETAR
-
-    return 0;
+    return res;
 }
 
 char* superConcatenateWithSep(char* v[], int size, char* s) {
+    int totalSize = 0;
+    int resIndex = 0;
+    for (int i=0;i<size;i++){
+        totalSize += len(v[i]); // Saco la longitud total necesaria
+    }
+    totalSize += len(s) * (size -1);
+    char* res = (char*) malloc(sizeof(char) * (totalSize));
+    for (int i=0;i<size;i++){
+        char* strActual = v[i];
+        for(int j = 0; strActual[j] != '\0'; j++, resIndex++){
+            res[resIndex] = strActual[j]; // Loop para añadir el string actual al resultado.
+        }
+        if (i < size - 1) {
+            for (int k = 0; s[k] != '\0'; k++, resIndex++) {
+                res[resIndex] = s[k];
+            }
+        }
+    }
 
-    // COMPLETAR
-
-    return 0;
+    return res;
 }
 
 int main() {
     /*
-    // -- Descomentar para probar --
-    // Lo siguiente es un ejemplo y DEBE ser modificado.
+    
 
-    char* v[5] = {"hola", "como", "va", "el", "dia"};
+    // */
+    char* v[5] = {"hola", "cosmo", "va", "el", "dia"};
 
     char* l = longest(v, 5);
 
@@ -43,8 +82,5 @@ int main() {
 
     free(sc);
     free(scs);
-
-    // */
-
     return 0;
 }
